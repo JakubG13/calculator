36bb2ed1cc3f6f60c879a551f79ee91e00482608
Jakub Gaj <gaju2001@o2.pl>
2023-04-13 20:16:43 +0200

    create delete feature

--- js/calculator.js
+++ js/calculator.js
@@ -7,12 +7,29 @@ let firstNumber = 0;
 let flag = true;
 let markFlag = true;
 let sign = 0;
+let percent = false;
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     screen.textContent += btn.textContent;
     number += btn.textContent;
   });
 });
+
+document
+  .querySelector(".calculator__btn--special3")
+  .addEventListener("click", () => {
+    number = number + "";
+    number = [...number];
+    index = number.length - 1;
+    number.splice(index, 1);
+    number = number.join("");
+    screen.textContent = "";
+    if (firstNumber) {
+      screen.textContent += firstNumber;
+      screen.textContent += sign;
+    }
+    screen.textContent += number;
+  });
 
 marks.forEach((mark) => {
   mark.addEventListener("click", () => {
@@ -34,6 +51,13 @@ marks.forEach((mark) => {
 equal.addEventListener("click", () => {
   flag = true;
   markFlag = true;
+  if (percent === true && (sign === "+" || sign === "-")) {
+    multiplier = number / 100;
+    number = multiplier * firstNumber;
+  } else if (percent === true && (sign === "/" || sign === "*")) {
+    multiplier = number / 100;
+    number = multiplier;
+  }
   if (sign === "+") {
     screen.textContent = +firstNumber + +number;
     number = +firstNumber + +number;
@@ -56,6 +80,7 @@ equal.addEventListener("click", () => {
     sign = "";
   }
   console.log(firstNumber, sign, number);
+  percent = false;
 });
 
 document
@@ -75,4 +100,12 @@ document
     markFlag = true;
     sign = 0;
     screen.textContent = "";
+    percent = false;
+  });
+
+document
+  .querySelector(".calculator__btn--special1")
+  .addEventListener("click", () => {
+    screen.textContent += "%";
+    percent = true;
   });
